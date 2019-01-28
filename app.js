f5d77f3522740938989de645fc19d30a82db7c30
octoccoper <octocopper@gmail.com>
2019-01-28 16:58:56 +0200

    modified app.js

--- app.js
+++ app.js
@@ -5,29 +5,46 @@ const User = function (name) {
 
 User.prototype = {
   send: function (message, to) {
-    this.message.send(message, this, to);
+    this.chatroom.send(message, this, to);
   },
   receive: function (message, from) {
-    console.log(`${from.name} to ${this.name}: message`);
+    console.log(`${from.name} to ${this.name}: ${message}`);
   }
-
 }
 
 const Chatroom = function () {
   let users = {};
 
   return {
-    register: function () {
-      users[users.name] = user;
+    register: function (user) {
+      users[user.name] = user;
       user.chatroom = this;
     },
     send: function (message, from, to) {
       if (to) {
         // Single user message
         to.receive(message, from);
-      } else { 
-        
+      } else {
+        for (key in users) {
+          if (users[key] !== from) {
+            users[key].receive(message, from);
+          }
+        }
       }
     }
   }
 }
+
+const user1 = new User("User1");
+const user2 = new User("User2");
+const user3 = new User("User3");
+
+const chatroom = new Chatroom();
+
+chatroom.register(user1);
+chatroom.register(user2);
+chatroom.register(user3);
+
+user1.send("Hi there!", user2);
+user2.send("Hello m*kers!!!");
+user3.send("Aloha", user2);
